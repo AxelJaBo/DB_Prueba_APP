566db7866be07d15fdb377163e5af555b4e5b830
AxelJaBo <axel.910921@gmail.com>
2024-01-30 17:08:49 -0800

    Agregar archivos de proyecto.

---
+++ DB_Prueba_APP/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Prueba_APP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-KA53HTR;Initial Catalog=DB_prueba;Persist Security Info=True;User ID=sa;Password=7GbE6:cX1NQG");
+
+
+        void getProducts()
+        {
+            SqlCommand c = new SqlCommand("exec Reporte_Producto_SP", con);
+            SqlDataAdapter sd = new SqlDataAdapter(c);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getProducts();
+        }
+    }
+}
